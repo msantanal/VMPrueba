e1d1ff3d2abfa5cb811f490cb8e1826eb167ab76
msantanal <108295791+msantanal@users.noreply.github.com>
2022-07-01 12:29:33 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,39 @@
+data "azurerm_resource_group" "RG" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_network_interface" "NIC" {
+  name                = var.name_nic
+  location            = data.azurerm_resource_group.RG.location
+  resource_group_name = data.azurerm_resource_group.RG.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "VM_windows_2019" {
+  name                = var.name_vm_windows_2019
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  size                = "Standard_F2"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.NIC.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.version
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,31 @@
+variable "resource_group_name" {
+  type        = string
+}
+variable "location" {
+  type        = string
+}
+variable "name_nic" {
+  type        = string
+}
+variable "name_vm_windows_2019" {
+  type        = string
+}
+
+variable "admin_username" {
+  type        = string
+}
+variable "admin_password" {
+  type        = string
+}
+variable "publisher" {
+  type        = string
+}
+variable "offer" {
+  type        = string
+}
+variable "sku" {
+  type        = string
+}
+variable "version" {
+  type        = string
+}
