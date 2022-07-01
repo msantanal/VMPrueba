8a0428d43d68f26d14fa2f2bca51d72eb8e85b99
msantanal <108295791+msantanal@users.noreply.github.com>
2022-07-01 17:26:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "RG" {
 }
 
 data "azurerm_virtual_network" "Vnet" {
-  name                = var.name_vnet
+  name                = "vnetpocpruebae353"
   resource_group_name = data.azurerm_resource_group.RG.name
 }
 
